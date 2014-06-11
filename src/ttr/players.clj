9ac96490ce61a8bdc62747be09ae6d595bb032fb
Matt DeBoard <matt.deboard@gmail.com>
2014-06-10 22:39:49 -0400

    Refactor ttr.players to tidy up state management.
    Factor all the state manipulation out into concise functions that
    do one thing.
    
    Refers #1

--- src/ttr/players.clj
+++ src/ttr/players.clj
@@ -1,5 +1,7 @@
 (ns ttr.players
-  (:require [ttr.cards :refer [discard-deck]]))
+  (:require [ttr.cards :refer :all]))
+
+(def starting-pcs 45)
 
 (defn new-player
   "Create a hashmap for a new player."
@@ -8,10 +10,34 @@
    :deck (ref [])
    :destinations (ref [])
    :points (ref 0)
-   :pieces-count (ref 45)
+   :pieces-count (ref starting-pcs)
    :pieces-color color})
 
-(defn update-player!
+(defn ref-inc!
+  "Increase the value of a ref, which is an element in hash map `player'
+with key `k' with a numerical value."
+  [n player ^clojure.lang.Keyword k]
+  (dosync
+   (let [s (get player k)]
+     (alter s + n))))
+
+(defn ref-dec!
+  "Decrease the value of a ref, which is an element in hash map `player'
+with key `k', with a numerical value."
+  [n player ^clojure.lang.Keyword k]
+  (dosync
+   (let [s (get player k)]
+     (alter s - n))))
+
+(defn ref-set!
+  "Set the value of a ref, which is an element in hash map `player'
+with key `k', to value `v'."
+  [v player ^clojure.lang.Keyword k]
+  (dosync
+   (let [s (get player k)]
+     (ref-set s v))))
+
+(defn update-player
   "Update player state after an action.
 
 This includes decrementing their deck & train set for any moves made, as well
@@ -29,17 +55,17 @@ Also, the discard pile is updated to include any newly used cards."
                         (apply concat))
         discards (filter #(= color %) @deck)
         num-left (- in-hand cost)]
-    (dosync
-     (ref-set deck (concat stay-cards (repeat num-left color)))
-     (alter discard-deck concat discards)
-     (alter pieces-count - cost)
-     (alter points + score))))
+    (do
+      (deck-set! deck (concat stay-cards (repeat num-left color)))
+      (deck-put! discard-deck discards)
+      (ref-inc! score player :points)
+      (ref-dec! cost player :pieces-count))))
 
-(defn reset-player! [player]
+(defn reset-player
+  [player]
   (let [{:keys [deck points pieces-count destinations]} player]
-    (dosync
-     (ref-set deck [])
-     (ref-set destinations [])
-     (ref-set points 0)
-     (ref-set pieces-count 45))))
-
+    (do
+      (deck-set! [] deck)
+      (deck-set! [] destinations)
+      (ref-set! :points 0 player)
+      (ref-set! :pieces-count starting-pcs))))
